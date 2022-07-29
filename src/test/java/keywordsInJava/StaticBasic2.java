38f2d2f419dd69f3d51b2556e6c6797defc9cf51
NavaranjithR <navaranjithr0312@gmail.com>
2022-07-29 23:38:11 +0530

    new

--- src/test/java/keywordsInJava/StaticBasic2.java
+++ src/test/java/keywordsInJava/StaticBasic2.java
@@ -3,8 +3,13 @@ package keywordsInJava;
 public class StaticBasic2 {
 	
 	
+public StaticBasic2() {
+		
+	}
+	
 	int a = 100;
 	static int b = 200;
+	String str = "ABC";
 	
 	public void method1() {
 		System.out.println("I am a non-static, non parameterized method");
@@ -53,6 +58,8 @@ public class StaticBasic2 {
 		System.out.println(obj1.a);
 		System.out.println(b);
 		System.out.println(StaticBasic.b);
+		
+		System.out.println(obj2.str.length());
 	}
 
 }
