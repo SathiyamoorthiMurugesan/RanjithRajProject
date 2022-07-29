61497f2c35edd22da137eb745453181c8f2239da
NavaranjithR <navaranjithr0312@gmail.com>
2022-07-29 23:35:17 +0530

    Interface Learning

---
+++ src/test/java/interfaceInJava/Interface2.java
@@ -0,0 +1,10 @@
+package interfaceInJava;
+
+public interface Interface2 extends LearnInterface {
+
+    public void test1(); 
+	
+	public void test2();
+
+}
+
---
+++ src/test/java/interfaceInJava/TestClass.java
@@ -0,0 +1,28 @@
+package interfaceInJava;
+
+public class TestClass implements Interface2{
+
+	public static void main(String[] args) {
+		LearnInterface obj1 = new TestClass();
+		Interface2 obj2 = new TestClass();
+		TestClass obj3 = new TestClass();
+		obj3.rule3();
+		obj1.rule3();
+	}
+
+	@Override
+	public void rule1() {
+		LearnInterface.rule2();
+	}
+
+	@Override
+	public void test1() {
+		
+	}
+
+	@Override
+	public void test2() {
+		// TODO Auto-generated method stub
+		
+	}
+}
